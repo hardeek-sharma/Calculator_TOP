6447c87e38761eb8e80865695d631a87992f04a7
hardeek-sharma <hardeeksa@gmail.com>
2025-12-31 11:58:22 -0500

    Add event listener for button click

--- javascript.js
+++ javascript.js
@@ -30,3 +30,75 @@ function operate(num1, operator, num2) {
       return divide(num1, num2);
   }
 }
+
+let screen = document.querySelector('#screen');
+let keypad = document.querySelector('#keypad');
+let clearBtn = document.querySelector('#clear');
+let deleteBtn = document.querySelector('#delete');
+
+keypad.addEventListener('click', (e) => {
+  const button = e.target.closest('.button');
+  switch (button.id) {
+    // Functions
+    case 'clear':             // Clear
+      screen.textContent = '';
+      break;
+    case 'delete':              // Delete
+      screen.textContent = screen.textContent.slice(0, -1);
+      break;
+
+    // Numbers & Dot
+    case 'zero':
+      screen.textContent += '0';
+      break;
+    case 'one':
+      screen.textContent += '1';
+      break;
+    case 'two':
+      screen.textContent += '2';
+      break;
+    case 'three':
+      screen.textContent += '3';
+      break;
+    case 'four':
+      screen.textContent += '4';
+      break;
+    case 'five':
+      screen.textContent += '5';
+      break;
+    case 'six':
+      screen.textContent += '6';
+      break;
+    case 'seven':
+      screen.textContent += '7';
+      break;
+    case 'eight':
+      screen.textContent += '8';
+      break;
+    case 'nine':
+      screen.textContent += '9';
+      break;
+    case 'dot':
+      screen.textContent += '.';
+      break;
+
+    // Operators
+    case 'addition':            // Addition
+      screen.textContent += '+';
+      break;
+    case 'subtraction':         // Subtraction
+      screen.textContent += '-';
+      break;
+    case 'multiplication':      // Multiplication
+      screen.textContent += '*';
+      break;
+    case 'division':            // Division
+      screen.textContent += '/';
+      break;
+    case 'equals':              // Equals
+      screen.textContent += '=';
+      break;
+  }
+
+  screen.scrollLeft = screen.scrollWidth;
+})
